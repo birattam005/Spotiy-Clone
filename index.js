c443e8eee3738ecc34fd81084730a512bd7d142e
Birattam Rajak <birattam444@gmail.com>
2023-03-30 19:51:35 +0530

    adding gif play paused

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let songIndex = 0;
 let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
 
 let songs = [
     // array of objects
@@ -20,11 +21,13 @@ masterPlay.addEventListener('click', ()=>{
         audioElement.play();
         masterPlay.classList.remove('fa-circle-play');
         masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity=1;
     }
     else{
         audioElement.pause();
         masterPlay.classList.add('fa-circle-play');
         masterPlay.classList.remove('fa-circle-pause');
+        gif.style.opacity=0;
     }
 })
 
